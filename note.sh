8fff4a3a78af340a8ad3c1189e83ad067c5b56f0
Gustav Nordqvist <gus.nordqvist@gmail.com>
2025-01-14 15:26:13 -0700

    added add function

--- note.sh
+++ note.sh
@@ -1,1 +1,21 @@
 #!/bin/bash
+NOTES_TXT=notes.txt
+
+function add() {
+    local note="$1"
+    echo "Your note text here: $1"
+    modified_note="$(date +"%Y-%m-%d %H:%M:%S") - $1"
+    echo "$modified_note" >> /notes.txt
+}
+
+
+
+function list() {
+    echo $NOTES_TXT
+}
+
+
+
+function search() {
+
+}
