53a83f8b770e7204dcec26f075b15573d0cd16f1
Gustav Nordqvist <gus.nordqvist@gmail.com>
2025-01-15 15:29:53 -0700

    updates

--- note.sh
+++ note.sh
@@ -3,22 +3,39 @@ NOTES_TXT=notes.txt
 
 function add() {
     local note="$1"
-    echo "Your note text here: $1"
+    echo "Note added successfully"
     modified_note="$(date +"%Y-%m-%d %H:%M:%S") - $1"
-    echo "$modified_note" >> /notes.txt
+    echo "$modified_note" >> notes.txt
+    exit 0
 }
 
 
 
 function list() {
-   # if $NOTES_TXT empty, then echo error and return 1 and fi for finished
-
-
-    echo $NOTES_TXT
-}
+    if [ ! -f "notes.txt" ]; then
+        echo "Error"
+        exit 1
+    else
+        cat $NOTES_TXT
+    fi
+}   
 
 
 
 function search() {
-
+    local search_word="$2"
+    grep -i "$search_word" notes.txt
 }
+
+function clear() {
+    > notes.txt
+}
+
+
+case "$1" in
+    add) add "$2" ;;
+    list) list ;;
+    search) search ;;
+    clear) clear ;;
+    *) echo "Invalid command"; exit 1 ;;
+esac
