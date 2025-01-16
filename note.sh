33edb9a82a9d7041c3d59f9b895fcb6708a55601
GustavNord <gus.nordqvist@gmail.com>
2025-01-15 22:47:52 -0700

    Update note.sh

--- note.sh
+++ note.sh
@@ -3,6 +3,10 @@ NOTES_TXT=notes.txt
 
 function add() {
     local note="$1"
+    if [[ -z "$note" ]], then
+        echo "Error: Note text cannot be empty."
+        exit 1
+    fi
     echo "Note added successfully"
     modified_note="$(date +"%Y-%m-%d %H:%M:%S") - $1"
     echo "$modified_note" >> notes.txt
@@ -12,19 +16,29 @@ function add() {
 
 
 function list() {
-    if [ ! -f "notes.txt" ]; then
-        echo "Error"
+    if [[ ! -f "$NOTES_TXT" || ! -s "$NOTES_TXT" ]]; then
+        echo "Error: No notes found"
         exit 1
     else
         cat $NOTES_TXT
+        exit 0
     fi
 }   
 
 
 
 function search() {
+    if [[ ! -f "$NOTES_TXT" ]], then
+        echo "Error: Notes file not found."
+        exit 1
+    fi
     local search_word="$2"
-    grep -i "$search_word" notes.txt
+    if [[ -z "$search_word" ]], then
+        echo "Error: Search keyword cannot be empty."
+        exit 1
+    fi
+    grep -i "$search_word" "$NOTES_TXT" || echo "No matching notes found."
+    exit 0
 }
 
 function clear() {
